15048358c01644defc916ae2febd540888e74f4b
krishna_kadam <krishna.kadam@ongil.io>
2022-02-06 19:12:46 +0530

    adding allow host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-^4!e9xy7w26&0l8hli_0aj*()ohies)y(wrp-=%t3%a+$r%mz3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'www.krisskad.in', 'krisskad-heroku.app', 'localhost']
 
 CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
