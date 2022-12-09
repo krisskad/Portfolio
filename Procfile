8bac51be8fae466ffbca44834b774d714c7e7790
krishna_kadam <krishna.kadam@ongil.io>
2022-12-09 14:47:49 +0530

    static files proc

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn portfolio.wsgi --log-file -
+web: python manage.py collectstatic --noinput && web: gunicorn portfolio.wsgi
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -35,8 +35,8 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    # url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
     path('sitemap.xml', sitemap, {"sitemaps":sitemaps})
 
